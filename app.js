0caf6efb02b36d848612e078b3579f8da53813c4
Allison01~ <farrella95@gmail.com>
2023-06-15 16:24:28 -0400

    queryselectors

--- app.js
+++ app.js
@@ -1,15 +1,69 @@
 let btn = document.querySelector('button');
-let monster = document.querySelector('img');
+let monsterSearch = document.querySelector('.monster-search').value;
+let monsterImg = document.querySelector('img');
+let monsterTitle = document.querySelector('.monster-title');
+let monsterAlignment = document.querySelector('.alignment');
+let monsterArmorClass = document.querySelector('.armor-class');
+let monsterChallengeRating = document.querySelector('.challenge-rating');
+let monsterCharisma = document.querySelector('.charisma');
+let monsterConditionImmunities = document.querySelector('.condition-immunities');
+let monsterConstitution = document.querySelector('.constitution');
+let monsterDamageImmunities = document.querySelector('.damage_immunities');
+let monsterDamageResistance = document.querySelector('.damage-resistance');
+let monsterDamageVulnerabilities = document.querySelector('.damage-vulnerabilities');
+let monsterDex = document.querySelector('.dexterity');
+let monsterHitDice= document.querySelector('.hit-dice');
+let monsterHitPoints = document.querySelector('.hit-points');
+let monsterHitPointsRoll = document.querySelector('.hit-points-roll');
+let monsterIntelligence = document.querySelector('.intelligence');
+let monsterLanguages = document.querySelector('.languages');
+let monsterLegendaryActions = document.querySelector('.legendary-actions');
+let monsterProficiencies = document.querySelector('.proficiencies');
+let monsterSenses = document.querySelector('.senses');
+let monsterSize = document.querySelector('.size');
+let monsterSpecialAbilities = document.querySelector('.special-abilities');
+let monsterSpeed = document.querySelector('.speed');
+let monsterStrength = document.querySelector('.strength');
+let monsterType = document.querySelector('.type');
+let monsterWisdom = document.querySelector('.wisdom');
+let monsterXp = document.querySelector('.xp');
 
 btn.addEventListener('click', getFetch())
 
 function getFetch() {
-    const index = 'aboleth'
-    const url = `https://www.dnd5eapi.co/api/monsters/${index}`;
+    const monsterIndex = monsterSearch;
+    const url = `https://www.dnd5eapi.co/api/monsters/${monsterIndex}`;
     fetch(url)
       .then((res) => res.json()) // parse response as JSON
       .then((data) => {
         console.log(data);
+        // monsterImg.src = data.image
+        monsterTitle.innerHTML = data.name;
+        monsterAlignment.innerHTML = data.alignment;
+        monsterArmorClass.innerHTML = data.armor_class;
+        monsterChallengeRating.innerHTML = data.challenge_rating;
+        monsterCharisma.innerHTML = data.charisma;
+        monsterConditionImmunities.innerHTML = data.condition_immunities;
+        monsterConstitution.innerHTML = data.constitution;
+        // monsterDamageImmunities.innerHTML= data.damage_immunities;
+        // monsterDamageResistance.innerHTML = data.damage_resistances;
+        // monsterDamageVulnerabilities.innerHTML = data.damage_vulnerabilities;
+        monsterDex.innerHTML = data.dexterity;
+        monsterHitDice.innerHTML = data.hit_dice;
+        monsterHitPoints.innerHTML = data.hit_points;
+        monsterHitPointsRoll.innerHTML = data.hit_points_roll;
+        monsterIntelligence.innerHTML = data.intelligence;
+        // monsterLanguages.innerHTML = data.languages;
+        // monsterLegendaryActions.innerHTML = data.legendary_actions;
+        // monsterProficiencies.innerHTML = data.proficiencies;
+        // monsterSenses.innerHTML = data.senses;
+        monsterSize.innerHTML = data.size;
+        // monsterSpecialAbilities.innerHTML = data.special_abilities;
+        monsterSpeed.innerHTML = data.speed;
+        monsterStrength.innerHTML = data.strength;
+        monsterType.innerHTML = data.type;
+        monsterWisdom.innerHTML = data.wisdom;
+        monsterXp.innerHTML = data.xp;
       })
     
       .catch((err) => {
