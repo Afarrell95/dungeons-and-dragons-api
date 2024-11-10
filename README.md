d53e2a28f3201961403b38cce1ff7718b0a5921f
Allison <farrella95@gmail.com>
2024-11-09 20:30:19 -0500

    added domcontentload to ensure dom loads before script run. Added a clear function to clear out old data on new search

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 # Description
 
-This is a fun little app created with just HTML, CSS, JavaScript, and utilizes the dnd5e API. Users can search for information on certain Dungeons & Dragons monsters by typing the name in the search bar. For example, if you type in "adult red dragon", the stats for that monster will show.
+This is a fun little app created with just HTML, CSS, JavaScript, and utilizes the dnd5e API. Users can search for information on 5th edition Dungeons & Dragons monsters by typing the name in the search bar. For example, if you type in "adult red dragon", the stats for that monster will show.
 
 # ToDos
 
--- app.js
+++ app.js
@@ -1,153 +1,171 @@
-let btn = document.querySelector("button");
-let monsterSearch = document.querySelector("input");
-let monsterImg = document.querySelector("img");
-let monsterTitle = document.querySelector(".monster-title");
-let monsterActions = document.querySelector(".monster-actions");
-let monsterAlignment = document.querySelector(".alignment");
-let monsterArmorClass = document.querySelector(".armor-class");
-let monsterChallengeRating = document.querySelector(".challenge-rating");
-let monsterCharisma = document.querySelector(".charisma");
-let monsterConditionImmunities = document.querySelector(
-  ".condition-immunities"
-);
-let monsterConstitution = document.querySelector(".constitution");
-let monsterDamageImmunities = document.querySelector(".damage_immunities");
-let monsterDamageResistance = document.querySelector(".damage-resistance");
-let monsterDamageVulnerabilities = document.querySelector(
-  ".damage-vulnerabilities"
-);
-let monsterDex = document.querySelector(".dexterity");
-let monsterHitDice = document.querySelector(".hit-dice");
-let monsterHitPoints = document.querySelector(".hit-points");
-let monsterHitPointsRoll = document.querySelector(".hit-points-roll");
-let monsterIntelligence = document.querySelector(".intelligence");
-let monsterLanguages = document.querySelector(".languages");
-let monsterLegendaryActions = document.querySelector(".legendary-actions");
-let monsterProficiencies = document.querySelector(".proficiencies");
-let monsterSenses = document.querySelector(".senses");
-let monsterSize = document.querySelector(".size");
-let monsterSpecialAbilities = document.querySelector(".special-abilities");
-let monsterSpeed = document.querySelector(".speed");
-let monsterStrength = document.querySelector(".strength");
-let monsterType = document.querySelector(".type");
-let monsterWisdom = document.querySelector(".wisdom");
-let monsterXp = document.querySelector(".xp");
+document.addEventListener("DOMContentLoaded", function () {
+  let btn = document.querySelector("button");
+  let monsterSearch = document.querySelector("input");
+  let monsterImg = document.querySelector("img");
+  let monsterTitle = document.querySelector(".monster-title");
+  let monsterActions = document.querySelector(".monster-actions");
+  let monsterAlignment = document.querySelector(".alignment");
+  let monsterArmorClass = document.querySelector(".armor-class");
+  let monsterChallengeRating = document.querySelector(".challenge-rating");
+  let monsterCharisma = document.querySelector(".charisma");
+  let monsterConstitution = document.querySelector(".constitution");
+  let monsterDex = document.querySelector(".dexterity");
+  let monsterHitDice = document.querySelector(".hit-dice");
+  let monsterHitPoints = document.querySelector(".hit-points");
+  let monsterHitPointsRoll = document.querySelector(".hit-points-roll");
+  let monsterIntelligence = document.querySelector(".intelligence");
+  let monsterLanguages = document.querySelector(".languages");
+  let monsterLegendaryActions = document.querySelector(".legendary-actions");
+  let monsterProficiencies = document.querySelector(".proficiencies");
+  let monsterSenses = document.querySelector(".senses");
+  let monsterSize = document.querySelector(".size");
+  let monsterSpecialAbilities = document.querySelector(".special-abilities");
+  let monsterSpeed = document.querySelector(".speed");
+  let monsterStrength = document.querySelector(".strength");
+  let monsterType = document.querySelector(".type");
+  let monsterWisdom = document.querySelector(".wisdom");
+  let monsterXp = document.querySelector(".xp");
 
-btn.addEventListener("click", getFetch);
+  btn.addEventListener("click", getFetch);
 
-function getFetch() {
-  const monsterIndex = monsterSearch.value.toLowerCase().split(" ").join("-");
-  const url = `https://www.dnd5eapi.co/api/monsters/${monsterIndex}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      // monsterImg.src = `https://www.dnd5eapi.co/${data.url}.png`;
-      monsterTitle.innerHTML = data.name;
+  function getFetch() {
+    const monsterIndex = monsterSearch.value.toLowerCase().split(" ").join("-");
+    const url = `https://www.dnd5eapi.co/api/monsters/${monsterIndex}`;
 
-      data.actions.forEach((actions) => {
-        let nameElement = document.createElement("span");
-        nameElement.textContent = actions.name;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        clearContent();
 
-        let descElement = document.createElement("p");
-        descElement.textContent = actions.desc;
+        monsterImg.src = `https://www.dnd5eapi.co/${data.url}.png`;
+        monsterTitle.innerHTML = data.name;
 
-        let attackBonusElement = document.createElement("p");
-        attackBonusElement.textContent =
-          "Attack Bonus: " + actions.attack_bonus;
+        data.actions.forEach((actions) => {
+          let nameElement = document.createElement("span");
+          nameElement.textContent = actions.name;
 
-        let damageElement = document.createElement("p");
-        damageElement.textContent = "Damage: " + actions.damage;
+          let descElement = document.createElement("p");
+          descElement.textContent = actions.desc;
 
-        monsterActions.appendChild(nameElement);
-        monsterActions.appendChild(descElement);
-        monsterActions.appendChild(attackBonusElement);
-        monsterActions.appendChild(damageElement);
+          let attackBonusElement = document.createElement("p");
+          attackBonusElement.textContent =
+            "Attack Bonus: " + actions.attack_bonus;
+
+          let damageElement = document.createElement("p");
+          damageElement.textContent = "Damage: " + actions.damage;
+
+          monsterActions.appendChild(nameElement);
+          monsterActions.appendChild(descElement);
+          monsterActions.appendChild(attackBonusElement);
+          monsterActions.appendChild(damageElement);
+        });
+
+        monsterAlignment.innerHTML = data.alignment;
+
+        data.armor_class.forEach((armor_class) => {
+          let typeElement = document.createElement("span");
+          typeElement.textContent = armor_class.type;
+
+          let valueElement = document.createElement("p");
+          valueElement.textContent = "Value: " + armor_class.value;
+
+          let armorDetailsElement = document.createElement("p");
+          armorDetailsElement.textContent = "Armor: " + armor_class.armor;
+
+          monsterArmorClass.appendChild(typeElement);
+          monsterArmorClass.appendChild(valueElement);
+          monsterArmorClass.appendChild(armorDetailsElement);
+        });
+
+        monsterChallengeRating.innerHTML = data.challenge_rating;
+        monsterCharisma.innerHTML = data.charisma;
+        monsterConstitution.innerHTML = data.constitution;
+        monsterDex.innerHTML = data.dexterity;
+        monsterHitDice.innerHTML = data.hit_dice;
+        monsterHitPoints.innerHTML = data.hit_points;
+        monsterHitPointsRoll.innerHTML = data.hit_points_roll;
+        monsterIntelligence.innerHTML = data.intelligence;
+        monsterLanguages.innerHTML = data.languages;
+
+        data.legendary_actions.forEach((legendary_action) => {
+          let nameElement = document.createElement("span");
+          nameElement.textContent = legendary_action.name;
+
+          let descElement = document.createElement("p");
+          descElement.textContent = legendary_action.desc;
+
+          monsterLegendaryActions.appendChild(nameElement);
+          monsterLegendaryActions.appendChild(descElement);
+        });
+
+        data.proficiencies.forEach((proficiency) => {
+          let nameElement = document.createElement("span");
+          nameElement.textContent = proficiency.proficiency.name;
+
+          let valueElement = document.createElement("p");
+          valueElement.textContent = "Value: " + proficiency.value;
+
+          monsterProficiencies.appendChild(nameElement);
+          monsterProficiencies.appendChild(valueElement);
+        });
+
+        let darkvisionElement = document.createElement("p");
+        darkvisionElement.textContent = "Darkvision: " + data.senses.darkvision;
+
+        let passivePerceptionElement = document.createElement("p");
+        passivePerceptionElement.textContent =
+          "Passive Perception: " + data.senses.passive_perception;
+
+        monsterSenses.appendChild(darkvisionElement);
+        monsterSenses.appendChild(passivePerceptionElement);
+        monsterSize.innerHTML = data.size;
+
+        data.special_abilities.forEach((special_ability) => {
+          let nameElement = document.createElement("span");
+          nameElement.textContent = special_ability.name;
+
+          let descElement = document.createElement("p");
+          descElement.textContent = "Description: " + special_ability.desc;
+
+          monsterSpecialAbilities.appendChild(nameElement);
+          monsterSpecialAbilities.appendChild(descElement);
+        });
+
+        monsterSpeed.innerHTML = data.speed;
+        monsterStrength.innerHTML = data.strength;
+        monsterType.innerHTML = data.type;
+        monsterWisdom.innerHTML = data.wisdom;
+        monsterXp.innerHTML = data.xp;
+      })
+      .catch((err) => {
+        console.log(`error ${err}`);
       });
-      monsterAlignment.innerHTML = data.alignment;
+  }
 
-      data.armor_class.forEach((armor_class) => {
-        let typeElement = document.createElement("span");
-        typeElement.textContent = armor_class.type;
-
-        let valueElement = document.createElement("p");
-        valueElement.textContent = "Value: " + armor_class.value;
-
-        let armorDetailsElement = document.createElement("p");
-        armorDetailsElement.textContent = "Armor: " + armor_class.armor;
-
-        monsterArmorClass.appendChild(typeElement);
-        monsterArmorClass.appendChild(valueElement);
-        monsterArmorClass.appendChild(armorDetailsElement);
-      });
-      monsterChallengeRating.innerHTML = data.challenge_rating;
-      monsterCharisma.innerHTML = data.charisma;
-
-      // monsterConditionImmunities.innerHTML = data.condition_immunities;
-
-      monsterConstitution.innerHTML = data.constitution;
-      // monsterDamageImmunities.innerHTML= data.damage_immunities;
-      // monsterDamageResistance.innerHTML = data.damage_resistances;
-      // monsterDamageVulnerabilities.innerHTML = data.damage_vulnerabilities;
-      monsterDex.innerHTML = data.dexterity;
-      monsterHitDice.innerHTML = data.hit_dice;
-      monsterHitPoints.innerHTML = data.hit_points;
-      monsterHitPointsRoll.innerHTML = data.hit_points_roll;
-      monsterIntelligence.innerHTML = data.intelligence;
-      monsterLanguages.innerHTML = data.languages;
-
-      data.legendary_actions.forEach((legendary_action) => {
-        let nameElement = document.createElement("span");
-        nameElement.textContent = legendary_action.name;
-
-        let descElement = document.createElement("p");
-        descElement.textContent = legendary_action.desc;
-
-        monsterLegendaryActions.appendChild(nameElement);
-        monsterLegendaryActions.appendChild(descElement);
-      });
-
-      data.proficiencies.forEach((proficiencies) => {
-        let nameElement = document.createElement("span");
-        nameElement.textContent = proficiencies.proficiency.name;
-
-        let valueElement = document.createElement("p");
-        valueElement.textContent = "Value: " + proficiencies.value;
-
-        monsterProficiencies.appendChild(nameElement);
-        monsterProficiencies.appendChild(valueElement);
-      });
-
-      let darkvisionElement = document.createElement("p");
-      darkvisionElement.textContent = "Darkvision: " + data.senses.darkvision;
-
-      let passivePerceptionElement = document.createElement("p");
-      passivePerceptionElement.textContent =
-        "Passive Perception: " + data.senses.passive_perception;
-
-      monsterSenses.appendChild(darkvisionElement);
-      monsterSenses.appendChild(passivePerceptionElement);
-      monsterSize.innerHTML = data.size;
-
-      data.special_abilities.forEach((special_abilities) => {
-        let nameElement = document.createElement("span");
-        nameElement.textContent = special_abilities.name;
-
-        let descElement = document.createElement("p");
-        descElement.textContent = "Description: " + special_abilities.desc;
-
-        monsterSpecialAbilities.appendChild(nameElement);
-        monsterSpecialAbilities.appendChild(descElement);
-      });
-
-      monsterSpeed.innerHTML = data.speed;
-      monsterStrength.innerHTML = data.strength;
-      monsterType.innerHTML = data.type;
-      monsterWisdom.innerHTML = data.wisdom;
-      monsterXp.innerHTML = data.xp;
-    })
-
-    .catch((err) => {
-      console.log(`error ${err}`);
-    });
-}
+  function clearContent() {
+    monsterImg.src = "";
+    monsterTitle.innerHTML = "";
+    monsterActions.innerHTML = "";
+    monsterLegendaryActions.innerHTML = "";
+    monsterProficiencies.innerHTML = "";
+    monsterSenses.innerHTML = "";
+    monsterSpecialAbilities.innerHTML = "";
+    monsterAlignment.innerHTML = "";
+    monsterArmorClass.innerHTML = "";
+    monsterChallengeRating.innerHTML = "";
+    monsterCharisma.innerHTML = "";
+    monsterConstitution.innerHTML = "";
+    monsterDex.innerHTML = "";
+    monsterHitDice.innerHTML = "";
+    monsterHitPoints.innerHTML = "";
+    monsterHitPointsRoll.innerHTML = "";
+    monsterIntelligence.innerHTML = "";
+    monsterLanguages.innerHTML = "";
+    monsterSize.innerHTML = "";
+    monsterSpeed.innerHTML = "";
+    monsterStrength.innerHTML = "";
+    monsterType.innerHTML = "";
+    monsterWisdom.innerHTML = "";
+    monsterXp.innerHTML = "";
+  }
+});
